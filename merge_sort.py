e06df179629dd338a051358dc4ee686f00127580
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2021-08-16 13:59:44 +0530

    updated merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -1,40 +1,44 @@
 # Python program for implementation of MergeSort
-def mergeSort(arr):
-	if len(arr) > 1:
-		mid = len(arr)//2
-		L = arr[:mid]
-		R = arr[mid:]
-		mergeSort(L)
-		mergeSort(R)
-		i = j = k = 0
-		while i < len(L) and j < len(R):
-			if L[i] < R[j]:
+class MergeSort:
+	def __init__(self,arr):
+		self.arr = arr
+		
+	def mergeSort(self,arr):
+		if len(arr) > 1:
+			mid = len(arr)//2
+			L = arr[:mid]
+			R = arr[mid:]
+			self.mergeSort(L)
+			self.mergeSort(R)
+			i = j = k = 0
+			while i < len(L) and j < len(R):
+				if L[i] < R[j]:
+					arr[k] = L[i]
+					i += 1
+				else:
+					arr[k] = R[j]
+					j += 1
+				k += 1
+			while i < len(L):
 				arr[k] = L[i]
 				i += 1
-			else:
+				k += 1
+
+			while j < len(R):
 				arr[k] = R[j]
 				j += 1
-			k += 1
-		while i < len(L):
-			arr[k] = L[i]
-			i += 1
-			k += 1
-
-		while j < len(R):
-			arr[k] = R[j]
-			j += 1
-			k += 1
-def printList(arr):
-	for i in range(len(arr)):
-		print(arr[i], end=" ")
-	print()
+				k += 1
+	def printList(self,arr):
+		for i in range(len(arr)):
+			print(arr[i])
 
 
 # Driver Code
 if __name__ == '__main__':
 	arr = [98,32,76,43,23]
-	print("Given array is", end="\n")
-	printList(arr)
-	mergeSort(arr)
-	print("Sorted array is: ", end="\n")
-	printList(arr)
+	print("Given array is \n")
+	merge = MergeSort(arr)
+	merge.printList(arr)
+	merge.mergeSort(arr)
+	print("Sorted array is:\n")
+	merge.printList(arr)
